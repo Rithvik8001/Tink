bcdc1ec54a3a5e25ad19ab4e235d3772f93e0b26
Rithvik8001 <1017rithvik@gmail.com>
2025-03-27 07:39:29 -0400

    Initial Commit

---
+++ Tink/TinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinkApp.swift
+//  Tink
+//
+//  Created by Rithvik Pallamreddy on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
